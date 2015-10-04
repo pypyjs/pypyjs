84dc5c6705814b550e976aa14398a78d69914891
Albert-Jan Nijburg <albertjan@curit.com>
2015-10-04 12:32:48 +0200

    fixes eslint and ads source maps back in

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,18 @@ var gulpFilter = require('gulp-filter');
 
 gulp.task('default', function def() {
   var filter = gulpFilter(['*', '!src/tests'], {restore: true});
-  
+
   return gulp.src('src/**/*.js')
     .pipe(eslint())
-//    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(filter) 
+    .pipe(eslint.format())
+    .pipe(filter)
     .pipe(wrapper({
       header: '(function() {\n',
       footer: '\nreturn pypyjs;\n}());'
     }))
     .pipe(filter.restore)
-//    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib'));
 });
